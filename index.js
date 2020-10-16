0e2c243d6ee4d9e51a53d983cabceaea14e7fefe
techAbhi1461 <tech1461abhi@gmail.com>
2020-10-16 14:09:04 -0400

    Added prevent default for our form

---
+++ index.js
@@ -0,0 +1,17 @@
+// JS variables
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const submitButton = document.querySelector('button');
+
+
+
+
+
+// Submit event listener
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    console.log(`Hi`);
+
+});
